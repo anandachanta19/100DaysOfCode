984b2f0f6f9d3ef8a4ca5173462e443c61c2190f
anandachanta19 <anandachanta19@gmail.com>
2024-05-28 11:28:54 +0530

    Create and move paddles

--- Pong Game/main.py
+++ Pong Game/main.py
@@ -1,16 +1,23 @@
 from turtle import Screen
-
+from paddles import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
-screen.setup(width=600, height=600)
+screen.setup(width=900, height=700)
+screen.title("Pong Game")
+screen.tracer(0)
+paddle_1 = Paddle()
+paddle_1.setpos(x=-440, y=0)
+paddle_2 = Paddle()
+paddle_2.setpos(x=435, y=0)
+screen.listen()
+screen.onkey(fun=paddle_2.up, key="Up")
+screen.onkey(fun=paddle_2.down, key="Down")
+screen.onkey(fun=paddle_1.up, key="w")
+screen.onkey(fun=paddle_1.down, key="s")
 
-
-
-
-
-
-
+is_game_on = True
+while is_game_on:
+    screen.update()
 
 screen.exitonclick()
-
---
+++ Pong Game/paddles.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.create_paddle()
+
+    def create_paddle(self):
+        self.pu()
+        self.shape("square")
+        self.shapesize(stretch_wid=1, stretch_len=5)
+        self.setheading(90)
+        self.color("white")
+        self.speed("fastest")
+
+    def up(self):
+        if self.heading() != 90:
+            self.setheading(90)
+        self.forward(40)
+
+    def down(self):
+        if self.heading() != 270:
+            self.setheading(270)
+        self.forward(40)
