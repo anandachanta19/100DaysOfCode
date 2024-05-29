5d11fb138201cb1692cff9f33951b96e6a10d9b6
anandachanta19 <anandachanta19@gmail.com>
2024-05-29 10:15:51 +0530

    Create Car

--- Turtle Crossing Game/main.py
+++ Turtle Crossing Game/main.py
@@ -1,7 +1,12 @@
 from turtle import Screen
+from car import Car
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.title("Turtle Crossing Game")
 screen.tracer(0)
+
+car = Car()
+screen.update()
+
 screen.exitonclick()
