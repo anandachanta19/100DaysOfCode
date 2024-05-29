5e2964c8b0fc8df4bc2cbcd51ebef7ea8c6cd738
anandachanta19 <anandachanta19@gmail.com>
2024-05-29 10:56:07 +0530

    Optimized Car Management

---
+++ Turtle Crossing Game/carManager.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+import random
+
+COLORS = ['red', 'green', 'blue', 'yellow', 'violet', 'pink', 'orange', 'brown']
+LANES = [-225, -200, -175, -150, -125, -100, -75, -50, -25, 0, 25, 50, 75, 100, 125, 150, 175, 200, 225]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager():
+
+    def __init__(self):
+        self.all_cars = []
+
+    def create_car(self):
+        probability_number = random.randint(1, 6)
+        if probability_number == 1:
+            new_car = Turtle()
+            new_car.shape("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.pu()
+            new_car.color(random.choice(COLORS))
+            new_car.setpos(x=300, y=random.choice(LANES))
+            new_car.setheading(180)
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.forward(STARTING_MOVE_DISTANCE)
--- Turtle Crossing Game/main.py
+++ Turtle Crossing Game/main.py
@@ -1,12 +1,19 @@
+import time
 from turtle import Screen
-from car import Car
+from carManager import CarManager
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.title("Turtle Crossing Game")
 screen.tracer(0)
+car_manager = CarManager()
 
-car = Car()
-screen.update()
+is_game_on = True
+while is_game_on:
+    time.sleep(0.1)
+    screen.update()
+    car_manager.create_car()
+    car_manager.move_cars()
+
 
 screen.exitonclick()
