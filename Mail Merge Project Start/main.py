d5ad9319779a43f828426de2d17ac406f6aec7d2
anandachanta19 <anandachanta19@gmail.com>
2024-06-01 10:15:06 +0530

    Implemented the requirements

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -1,12 +1,20 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
-
-with open("../Input/Letters/starting_letter.txt", 'r') as sample:
+# Getting Letter from the file
+with open("Input/Letters/starting_letter.txt", 'r') as sample:
     data = sample.read()
-    print(data)
+
+# Getting names from the names file
+with open('Input/Names/invited_names.txt', 'r') as name_file:
+    names = []
+    for line in name_file:
+        names.append(line)
+
+# Deleting the new line from the retrieved names
+for i in range(len(names)):
+    if i != len(names) - 1:
+        names[i] = names[i][0:len(names[i]) - 1]
+
+# Creating the modified letters
+for name in names:
+    with open(f'Output/ReadyToSend/letter_for_{name}.txt', 'w') as letter:
+        modified_letter = data.replace("[name]", name)
+        letter.write(modified_letter)
