f77e965528f8f52dbc140d838015643af66fefb5
anandachanta19 <anandachanta19@gmail.com>
2024-06-01 13:26:25 +0530

    Added states to learn data

--- U.S States Quiz/main.py
+++ U.S States Quiz/main.py
@@ -23,14 +23,24 @@ states = data["state"].tolist()
 x_cor = data["x"].tolist()
 y_cor = data["y"].tolist()
 
-count = 0
-while count < len(states):
-    answer = screen.textinput(title=f"{count}/50 States Correct", prompt="What's another state's name?")
+guessed_states = []
+while len(guessed_states) < 50:
+    answer = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?")
     answer = answer.title()
+
+    if answer == "Exit":
+        missing_states = []
+        for state in states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        missing_data = pandas.DataFrame(missing_states)
+        missing_data.to_csv("States to learn.csv")
+        break
+
     if answer in states:
+        guessed_states.append(answer)
         index = states.index(answer)
         write_state(x_cor[index], y_cor[index], states[index])
-        count += 1
     else:
         break
 screen.exitonclick()
