244d395d882ca91e6b5bd8e4c876918db52d4754
anandachanta19 <anandachanta19@gmail.com>
2024-06-01 11:14:22 +0530

    Completed Avg Temperature Challenge

--- Pandas CSV/main.py
+++ Pandas CSV/main.py
@@ -1,15 +1,21 @@
-import csv
+# import csv
+#
+# # Storing Temperatures
+# with open('weather_data.csv') as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     flag = True
+#     for row in data:
+#         if flag:
+#             flag = False
+#             continue
+#         temperature = int(row[1])
+#         temperatures.append(temperature)
+#
+# print(temperatures)
 
-# Storing Temperatures
-with open('weather_data.csv') as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    flag = True
-    for row in data:
-        if flag:
-            flag = False
-            continue
-        temperature = int(row[1])
-        temperatures.append(temperature)
+import pandas
 
-print(temperatures)
+data = pandas.read_csv("weather_data.csv")
+temperatures = data["temp"].tolist()
+print(f"Average Temperature: {round(sum(temperatures)/len(temperatures), 2)}")
