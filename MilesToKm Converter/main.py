fd3f72a2f1f1836f59163ec7fecf22d481b58dd9
anandachanta19 <anandachanta19@gmail.com>
2024-06-03 20:17:42 +0530

    Created a Mile to km converter

--- MilesToKm Converter/main.py
+++ MilesToKm Converter/main.py
@@ -1,11 +1,28 @@
 import tkinter
 
+MILE = 1.609
+
+
+def calculate():
+    result["text"] = f"{round(float(user_input.get()) * MILE, 2)}"
+
+
 window = tkinter.Tk()
-window.title("My First GUI Program")
-window.minsize(width=500, height=300)
+window.title("Mile to KM Converter")
+window.config(padx=20, pady=20)
+# window.minsize(width=400, height=400)
 
-my_label = tkinter.Label(text="I am Anand")
-my_label.pack()
-
+miles = tkinter.Label(text="Miles", font=("Courier", 13, "bold"))
+miles.grid(row=0, column=2)
+is_equal = tkinter.Label(text="is equal to", font=("Courier", 13, "bold"))
+is_equal.grid(row=1, column=0)
+result = tkinter.Label(text="", font=("Courier", 13, "bold"))
+result.grid(row=1, column=1)
+km = tkinter.Label(text="Km", font=("Courier", 13, "bold"))
+km.grid(row=1, column=2)
+button = tkinter.Button(text="Calculate", command=calculate)
+button.grid(row=2, column=1)
+user_input = tkinter.Entry(width=7)
+user_input.grid(row=0, column=1)
 
 window.mainloop()
