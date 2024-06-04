b7c5b70e05621c195b58f8c828695b01c216da22
anandachanta19 <anandachanta19@gmail.com>
2024-06-04 18:05:45 +0530

    Enhance UI

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -3,28 +3,56 @@ import tkinter
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
-GREEN = "#9bdeac"
+GREEN = "#94B0A2"
 YELLOW = "#f7f5dd"
-FONT_NAME = "Arial"
+DARK = "#2F4858"
+FONT_NAME = "Impact"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+CHECK = "✔"
 
-# ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset():
+    pass
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start():
+    pass
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = tkinter.Tk()
 window.title("Pomodoro")
-# window.minsize(width=600, height=600)
-window.config(padx=100, pady=100, bg=YELLOW)
+window.config(padx=100, pady=100, bg=DARK)
 
-canvas = tkinter.Canvas(width=220, height=223, bg=YELLOW, highlightthickness=0)
+# State
+timer = tkinter.Label(text="TIMER", fg=YELLOW, bg=DARK, font=(FONT_NAME, 30, "bold"))
+timer.grid(row=0, column=1)
+
+# Start
+start = tkinter.Button(text="START", command=start, font=(FONT_NAME, 20, "bold"), fg=GREEN, bg=DARK,
+                       highlightthickness=0)
+start.grid(row=2, column=0)
+
+# Reset
+reset = tkinter.Button(text="RESET", command=reset, font=(FONT_NAME, 20, "bold"), fg=GREEN, bg=DARK,
+                       highlightthickness=0)
+reset.grid(row=2, column=3)
+
+# Check
+check = tkinter.Label(text=CHECK, font=(FONT_NAME, 15, "bold"), fg="green", bg=DARK)
+check.grid(row=2, column=1)
+
+# Image
+canvas = tkinter.Canvas(width=220, height=223, bg=DARK, highlightthickness=0)
 photo = tkinter.PhotoImage(file="tomato.png")
 canvas.create_image(112, 111, image=photo)
-canvas.create_text(112, 130, text="00:00", fill=YELLOW, font=(FONT_NAME, 40, "bold"))
-canvas.pack()
+canvas.create_text(112, 130, text="00 : 00", fill=YELLOW, font=(FONT_NAME, 30, "bold"))
+canvas.grid(row=1, column=1)
+
 window.mainloop()
