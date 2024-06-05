01117764f5e7332079bac411d3fc822307e9244b
anandachanta19 <anandachanta19@gmail.com>
2024-06-05 12:03:51 +0530

    Implemented functionality to add

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -1,9 +1,23 @@
 import tkinter
+
 FONT = "Arial"
+EMAIL = "anandachanta19@gmail.com"
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def clear():
+    website_input.delete(0, "end")
+    password_input.delete(0, "end")
+
+
+def save():
+    with open("data.txt", "w") as file:
+        file.write(f"\n{website_input.get()} | {email_input.get()} | {password_input.get()}")
+    clear()
+    website_input.focus()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -27,19 +41,19 @@ password = tkinter.Label(text="Password:", font=(FONT, 10, "normal"))
 password.grid(row=3, column=0)
 
 # Field Inputs
-website_input = tkinter.Entry(width=50)
+website_input = tkinter.Entry(width=51)
 website_input.grid(row=1, column=1, columnspan=2)
-email_input = tkinter.Entry(width=50)
+website_input.focus()
+email_input = tkinter.Entry(width=51)
 email_input.grid(row=2, column=1, columnspan=2)
-password_input = tkinter.Entry(width=32)
+email_input.insert(0, EMAIL)
+password_input = tkinter.Entry(width=33)
 password_input.grid(row=3, column=1)
 
-
 # Buttons
-generate = tkinter.Button(text="Generate", borderwidth=1, relief="groove", width=14)
+generate = tkinter.Button(text="Generate", borderwidth=1, width=14)
 generate.grid(row=3, column=2)
-add = tkinter.Button(text="Add", width=43, borderwidth=1, relief="groove")
+add = tkinter.Button(text="Add", width=43, borderwidth=1, command=save)
 add.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
-
