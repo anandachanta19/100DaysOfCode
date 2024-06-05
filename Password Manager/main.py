0ef87c4783cf6f0e7e1eaa1ddcc230d47ec60b89
anandachanta19 <anandachanta19@gmail.com>
2024-06-05 11:43:51 +0530

    Enhanced UI

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -1,4 +1,5 @@
 import tkinter
+FONT = "Arial"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -9,11 +10,36 @@ import tkinter
 # Window Creation
 window = tkinter.Tk()
 window.title("Password Manager")
-window.config(pady=20, padx=20)
+window.config(pady=50, padx=50)
 
 # Logo
 canvas = tkinter.Canvas(width=200, height=200)
 photo = tkinter.PhotoImage(file="logo.png")
 logo = canvas.create_image(100, 100, image=photo)
-canvas.pack()
+canvas.grid(row=0, column=1)
+
+# Field Names
+website = tkinter.Label(text="Website:", font=(FONT, 10, "normal"))
+website.grid(row=1, column=0)
+email = tkinter.Label(text="Email/Username:", font=(FONT, 10, "normal"))
+email.grid(row=2, column=0)
+password = tkinter.Label(text="Password:", font=(FONT, 10, "normal"))
+password.grid(row=3, column=0)
+
+# Field Inputs
+website_input = tkinter.Entry(width=50)
+website_input.grid(row=1, column=1, columnspan=2)
+email_input = tkinter.Entry(width=50)
+email_input.grid(row=2, column=1, columnspan=2)
+password_input = tkinter.Entry(width=32)
+password_input.grid(row=3, column=1)
+
+
+# Buttons
+generate = tkinter.Button(text="Generate", borderwidth=1, relief="groove", width=14)
+generate.grid(row=3, column=2)
+add = tkinter.Button(text="Add", width=43, borderwidth=1, relief="groove")
+add.grid(row=4, column=1, columnspan=2)
+
 window.mainloop()
+
