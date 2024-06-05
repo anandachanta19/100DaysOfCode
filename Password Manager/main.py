7f87aafde930eabd4be7157881f593c06f13ac82
anandachanta19 <anandachanta19@gmail.com>
2024-06-05 10:48:11 +0530

    Inserted the Logo

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -1,5 +1,19 @@
+import tkinter
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+# Window Creation
+window = tkinter.Tk()
+window.title("Password Manager")
+window.config(pady=20, padx=20)
+
+# Logo
+canvas = tkinter.Canvas(width=200, height=200)
+photo = tkinter.PhotoImage(file="logo.png")
+logo = canvas.create_image(100, 100, image=photo)
+canvas.pack()
+window.mainloop()
