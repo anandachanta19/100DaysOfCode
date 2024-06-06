e90fd1274255d181a25bf5afd22bf8bc975378ae
anandachanta19 <anandachanta19@gmail.com>
2024-06-06 12:01:13 +0530

    Enhanced using try catch exceptions

--- NATO Alphabet Project/main.py
+++ NATO Alphabet Project/main.py
@@ -1,10 +1,24 @@
 import pandas
 
+codes = None
+
+
+def user_input():
+    # Taking user Input
+    global codes
+    name = input("Enter your name: ").upper()
+    try:
+        codes = [nato[letter] for letter in name]
+    except KeyError:
+        print("Sorry only alphabets are allowed!")
+        user_input()
+    finally:
+        return codes
+
+
 # Converting CSV to DataFrame
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 # Creating nato alphabet dictionary
 nato = {row.letter: row.code for (index, row) in data.iterrows()}
-# Taking user Input
-user_input = input("Enter your name: ").upper()
-result = [nato[letter] for letter in user_input]
+result = user_input()
 print(result)
