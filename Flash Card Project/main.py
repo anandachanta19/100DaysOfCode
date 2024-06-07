1ae011e11048fc5ede2e182c43b48433e3cb3ce0
anandachanta19 <anandachanta19@gmail.com>
2024-06-07 14:01:08 +0530

    Having track of words to be learned

--- Flash Card Project/main.py
+++ Flash Card Project/main.py
@@ -1,3 +1,4 @@
+import csv
 import tkinter
 import pandas
 import random
@@ -9,7 +10,8 @@ FONT_2 = ("Ariel", 60, "bold")
 # Working with data
 data = pandas.read_csv("data/french_words.csv")
 words = data.to_dict(orient="records")
-new_word ={}
+new_word = {}
+known_words = []
 
 
 def reveal():
@@ -19,8 +21,16 @@ def reveal():
     canvas.itemconfig(word, fill="white", text=new_word["English"])
 
 
-def generate_flashcard():
-    global new_word, flip_timer
+def generate_flashcard(status):
+    global new_word, flip_timer, words
+    if status:
+        try:
+            words.remove(new_word)
+        except ValueError:
+            pass
+        else:
+            new_data = pandas.DataFrame(words)
+            new_data.to_csv("data/words_to _learn.csv", index=False)
     window.after_cancel(flip_timer)
     new_word = random.choice(words)
     canvas.itemconfig(card_front, image=front)
@@ -48,9 +58,9 @@ right = tkinter.PhotoImage(file="images/right.png")
 wrong = tkinter.PhotoImage(file="images/wrong.png")
 # Image Buttons
 right_button = tkinter.Button(image=right, highlightthickness=0, relief="flat", borderwidth=0,
-                              command=generate_flashcard)
+                              command=lambda: generate_flashcard(True))
 right_button.grid(row=1, column=2)
 left_button = tkinter.Button(image=wrong, highlightthickness=0, relief="flat", borderwidth=0,
-                             command=generate_flashcard)
+                             command=lambda: generate_flashcard(False))
 left_button.grid(row=1, column=0)
 window.mainloop()
