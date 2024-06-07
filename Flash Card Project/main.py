ccb4f410742d3f764a161ba9a6e8f6dd8d303120
anandachanta19 <anandachanta19@gmail.com>
2024-06-07 13:03:43 +0530

    Create New Flashcards

--- Flash Card Project/main.py
+++ Flash Card Project/main.py
@@ -1,7 +1,22 @@
 import tkinter
+import pandas
+import random
+
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_1 = ("Ariel", 40, "italic")
 FONT_2 = ("Ariel", 60, "bold")
+
+# Working with data
+data = pandas.read_csv("data/french_words.csv")
+words = data.to_dict(orient="records")
+print(words)
+
+
+def generate_flashcard():
+    new_word = random.choice(words)
+    canvas.itemconfig(name, text="French")
+    canvas.itemconfig(word, text=new_word["French"])
+
 
 # UI Setup
 window = tkinter.Tk()
@@ -11,15 +26,16 @@ window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 canvas = tkinter.Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 front = tkinter.PhotoImage(file="images/card_front.png")
 card_front = canvas.create_image(400, 263, image=front)
-name = canvas.create_text(400, 150, text="French", font=FONT_1)
-word = canvas.create_text(400, 263, text="trouve", font=FONT_2)
+name = canvas.create_text(400, 150, text="Title", font=FONT_1)
+word = canvas.create_text(400, 263, text="word", font=FONT_2)
 canvas.grid(row=0, column=0, columnspan=3)
 
 right = tkinter.PhotoImage(file="images/right.png")
 wrong = tkinter.PhotoImage(file="images/wrong.png")
 # Image Buttons
-right_button = tkinter.Button(image=right, highlightthickness=0, relief="flat", borderwidth=0)
+right_button = tkinter.Button(image=right, highlightthickness=0, relief="flat", borderwidth=0,
+                              command=generate_flashcard)
 right_button.grid(row=1, column=2)
-left_button = tkinter.Button(image=wrong, highlightthickness=0, relief="flat", borderwidth=0)
+left_button = tkinter.Button(image=wrong, highlightthickness=0, relief="flat", borderwidth=0, command=generate_flashcard)
 left_button.grid(row=1, column=0)
 window.mainloop()
