dca0a9bfaafd0b54eaeaa0b17235651da18b7435
anandachanta19 <anandachanta19@gmail.com>
2024-06-17 11:06:22 +0530

    Created Pixela User

---
+++ Habit Tracker/main.py
@@ -0,0 +1,13 @@
+import requests
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv()
+# user_params = {
+#     "token": os.environ["TOKEN"],
+#     "username": os.environ["PIXELA_USERNAME"],
+#     "agreeTermsOfService": "yes",
+#     "notMinor": "yes"
+# }
+#
+# create_user = requests.post(url="https://pixe.la/v1/users", json=user_params)
