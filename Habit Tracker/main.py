f7f9e0483278ca484b1cf8547b30502c408f4f53
anandachanta19 <anandachanta19@gmail.com>
2024-06-17 11:56:45 +0530

    Added Pixel

--- Habit Tracker/main.py
+++ Habit Tracker/main.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv
+from datetime import datetime as dt
 
 load_dotenv()
 # user_params = {
@@ -21,12 +22,30 @@ graph_params = {
     "color": "shibafu",
     "timezone": "Asia/Kolkata",
 }
+
 headers = {
     "X-USER-TOKEN": os.environ["PIXELA_AUTHENTICATION_TOKEN"]
 }
+#
+# create_graph = requests.post(
+#     url=f"https://pixe.la/v1/users/{os.environ["PIXELA_USERNAME"]}/graphs",
+#     json=graph_params,
+#     headers=headers,
+# )
 
-create_graph = requests.post(
-    url=f"https://pixe.la/v1/users/{os.environ["PIXELA_USERNAME"]}/graphs",
-    json=graph_params,
+# ADDING PIXEL
+today_date = ""
+for part in str(dt.now().date()).split("-"):
+    today_date += part
+
+pixel_params = {
+    "date": today_date,
+    "quantity": "8",
+}
+
+add_pixel = requests.post(
+    url=f"https://pixe.la/v1/users/{os.environ["PIXELA_USERNAME"]}/graphs/{graph_params["id"]}",
+    json=pixel_params,
     headers=headers,
 )
+print(add_pixel.text)
