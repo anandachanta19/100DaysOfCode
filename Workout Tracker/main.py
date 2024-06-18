b155f81020f373eb432fd50a2adaacda325addf0
anandachanta19 <anandachanta19@gmail.com>
2024-06-18 11:47:07 +0530

    Successfully Tracking Workouts

--- Workout Tracker/main.py
+++ Workout Tracker/main.py
@@ -1,20 +1,27 @@
 import os
 from dotenv import load_dotenv
 import requests
+from datetime import datetime as dt
 
 load_dotenv()
 
 EXERCISE_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+SHEETY_ENDPOINT = os.environ["SHEETY_ENDPOINT"]
 APP_ID = os.environ["NUTRITIONIX_APP_ID"]
 API_KEY = os.environ["NUTRITIONIX_API_KEY"]
 MY_WEIGHT = 68  # KG
 MY_HEIGHT = 168  # CM
 AGE = 19  # YEARS
+SHEETY_TOKEN = os.environ["SHEETY_TOKEN"]
 
-HEADERS = {
+EXERCISE_HEADERS = {
     "x-app-id": APP_ID,
     "x-app-key": API_KEY,
     "x-remote-user-id": "0",
+}
+
+SHEETY_HEADERS = {
+    "Authorization": f"Bearer {SHEETY_TOKEN}"
 }
 
 parameters = {
@@ -24,5 +31,22 @@ parameters = {
     "age": AGE,
 }
 
-response = requests.post(url=EXERCISE_ENDPOINT, json=parameters, headers=HEADERS)
-print(response.json())
+response = requests.post(url=EXERCISE_ENDPOINT, json=parameters, headers=EXERCISE_HEADERS)
+response.raise_for_status()
+data = response.json()["exercises"]
+for exercise in data:
+    sheet_params = {
+        "workout": {
+            "date": f"{dt.now().strftime("%d/%m/%Y")}",
+            "time": f"{dt.now().strftime("%H:%M:%S")}",
+            "exercise": f"{exercise["name"]}",
+            "duration": f"{exercise["duration_min"]}",
+            "calories": f"{exercise["nf_calories"]}",
+        }
+    }
+    sheet_response = requests.post(
+        url=SHEETY_ENDPOINT,
+        json=sheet_params,
+        headers=SHEETY_HEADERS,
+    )
+    sheet_response.raise_for_status()
