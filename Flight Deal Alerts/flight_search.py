152a27ac1ac37042969decd0f5f0ddf62fa63ce0
anandachanta19 <anandachanta19@gmail.com>
2024-06-19 19:22:53 +0530

    Getting Access Token

--- Flight Deal Alerts/flight_search.py
+++ Flight Deal Alerts/flight_search.py
@@ -1,3 +1,32 @@
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+TOKEN_ENDPOINT = "https://test.api.amadeus.com/v1/security/oauth2/token"
+
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+    def __init__(self):
+        self.api_key = os.environ["FLIGHT_API_KEY"]
+        self.api_secret = os.environ["FLIGHT_API_SECRET"]
+        self.token = self.get_new_token()
+
+    def get_new_token(self):
+        header = {
+            "Content-Type": "application/x-www-form-urlencoded",
+        }
+        body = {
+            'grant_type': 'client_credentials',
+            'client_id': self.api_key,
+            'client_secret': self.api_secret
+        }
+
+        response = requests.post(url=TOKEN_ENDPOINT, headers=header, data=body)
+
+        print(f"Your token is {response.json()['access_token']}")
+        print(f"Your token expires in {response.json()['expires_in']} seconds")
+        return response.json()['access_token']
+
+    def get_iata_code(self):
+
--- Flight Deal Alerts/main.py
+++ Flight Deal Alerts/main.py
@@ -1,1 +1,6 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from flight_search import FlightSearch
+
+flights = FlightSearch()
+
+# This file will need to use the DataManager
+# ,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
