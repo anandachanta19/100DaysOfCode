55db5b772216d1aaaa0cb981969edf54192dd322
anandachanta19 <anandachanta19@gmail.com>
2024-06-22 19:54:22 +0530

    Extracting Top 100 songs

--- Top 100 Spotify Playlist/main.py
+++ Top 100 Spotify Playlist/main.py
@@ -1,1 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.billboard.com/charts/hot-100/"
+
 date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+url = URL + date
+response = requests.get(url=url)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+song_names_spans = soup.select("li ul li h3")
+song_names = [song.getText().strip() for song in song_names_spans]
+
