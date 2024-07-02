c5389a786bcb1a41539e1b5b3ec16113a75e2385
anandachanta19 <anandachanta19@gmail.com>
2024-07-02 18:34:10 +0530

    Start Files

--- Secrets Website/main.py
+++ Secrets Website/main.py
@@ -1,25 +1,31 @@
-from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import EmailField, PasswordField
+from wtforms.validators import DataRequired
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
-
-
+load_dotenv()
 app = Flask(__name__)
+app.secret_key = os.getenv("APP_SECRET_KEY")
+
+
+class MyForm(FlaskForm):
+    email = EmailField('email', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = MyForm()
+    if form.validate_on_submit():
+        return "Success"
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
