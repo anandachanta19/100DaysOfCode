dda596ab27cf98698bc6d5fd8c41a5d868134d56
anandachanta19 <anandachanta19@gmail.com>
2024-07-02 19:55:06 +0530

    Bootstrap-Flask

--- Secrets Website/main.py
+++ Secrets Website/main.py
@@ -1,18 +1,20 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
+from flask_bootstrap import Bootstrap5
 
 load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv("APP_SECRET_KEY")
+bootstrap = Bootstrap5(app)
 
 
-class MyForm(FlaskForm):
-    email = EmailField('email', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
+class LoginForm(FlaskForm):
+    email = EmailField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
 
 
 @app.route("/")
@@ -22,10 +24,13 @@ def home():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    form = MyForm()
-    if form.validate_on_submit():
-        return "Success"
-    return render_template('login.html', form=form)
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+    return render_template('login.html', form=login_form)
 
 
 if __name__ == '__main__':
